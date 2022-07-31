cc7d0ad385655f3b0e2f4220d5f44a3a853adf76
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2022-08-01 01:06:09 +0600

    Creating list with person type object.

--- main.dart
+++ main.dart
@@ -1,6 +1,32 @@
-main(){
+class Person{
+ late  String name;
+ Person(this.name);                                                 //constructor
+}
 
-  var list = [16,23,18,08,156,2365];
+main(){
+  var antor = new Person("Antor");
+  var dina = new Person("Dina");
+  var rifat  = new Person("Rifat");
+
+
+  var list = [16,23,18,08,156,2365]; 
+  var onlyString = <String>[];
+  var personList = <Person>[];
+
+ personList.add(antor);
+ personList.add(dina);
+ personList.add(rifat);
+  for(int i = 0; i <personList.length; i++)
+  {
+    print(personList[i].name);
+  }
+
+  onlyString.add("Opu");
+  onlyString.add("Hoimonti");
+  for(int i = 0; i <onlyString.length; i++)
+  {
+    print(onlyString[i]);
+  }
 
   print(list.length);
   print(list[4]);
